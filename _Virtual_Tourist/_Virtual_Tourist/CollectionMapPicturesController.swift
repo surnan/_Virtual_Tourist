f226f633224ef79fd5ae95d79841206bd554c98d
Suresh N <forsuresh@yandex.com>
2019-03-18 14:23:57 -0400

    Project Successfully starts programmatically.

---
+++ _Virtual_Tourist/_Virtual_Tourist/CollectionMapPicturesController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  _Virtual_Tourist
+//
+//  Created by admin on 3/18/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class CollectionMapPicturesController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.blue
+        
+    }
+
+
+}
+
